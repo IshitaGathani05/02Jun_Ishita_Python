206d1424f8e7cf37882550972c602cec8c9feeb6
IshitaGathani05 <ishitagathani5@gmail.com>
2025-09-18 21:08:54 +0530

    Add files via upload

---
+++ Assignments/practicalexample/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def p1(request):
+    return render(request,'p1.html')
+
+def pr_ex_1(request):
+    return render(request,'pr_ex_1.html')
